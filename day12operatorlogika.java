bf76f86c8cd3ab7579303de6469da709d2794c86
azisah <147221473+Azis4h@users.noreply.github.com>
2023-10-20 19:47:12 +0800

    day12operatorlogika.java

---
+++ day12operatorlogika.java
@@ -0,0 +1,22 @@
+
+package operatorlogika.java;
+public class OperatorlogikaJava {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        boolean c;
+        
+        //konjungsi (dan)
+        c = a && b;
+        System.out.println("true && false : "+c);
+        
+       //disjunsi (atau)
+       c = a || b;
+        System.out.println("true || false : "+c);
+        
+        // negasi (bukan)
+        System.out.println("false ! true : "+ !b);
+      
+    }
+    
+}
