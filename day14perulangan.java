9cab6d0bfc1352f933da0ccaca2b0210a415fe9e
azisah <147221473+Azis4h@users.noreply.github.com>
2023-10-22 17:31:20 +0800

    day14perulangan.java

---
+++ day14perulangan.java
@@ -0,0 +1,10 @@
+package perulangan;
+public class Perulangan {
+    public static void main(String[] args) {
+int a;
+       for(a =2; a<10; a += 2){
+           System.out.println("nilai a "+a);
+       }
+    }
+    
+}
