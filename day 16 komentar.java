f6f7bc1aa794438231ab83d90ee7a78c2b8c524f
azisah <147221473+Azis4h@users.noreply.github.com>
2023-10-24 20:29:11 +0800

    day 16 komentar.java

---
+++ day 16 komentar.java
@@ -0,0 +1,19 @@
+package komentar.java;
+public class KomentarJava {
+    public static void main(String[] args) {
+        //ini adalah komentar satu baris
+        // kode diluar blok kelas
+        /* contoh komentar dalam beberapa 
+            gaeis
+        */
+        /**
+         * komentar dokumtasi
+         * biasanya di gunakan untuk menjelaskan fungsi atau kelas
+         */
+        
+        System.out.println("jangan malas ngoding ica,ingat 100 itu lama ");
+        //sebagai pernyataan koding
+       
+    }
+    
+}
