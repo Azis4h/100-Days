0c6ea3f22e7b4761db832b04cbe674d67992f869
azisah <147221473+Azis4h@users.noreply.github.com>
2023-10-26 20:53:37 +0800

    day 18 operator.java

---
+++ day 18 operator.java
@@ -0,0 +1,19 @@
+package operator.java;
+public class OperatorJava {
+    public static void main(String[] args) {
+
+        long a = 21, d = 2;
+        long h = a%d;
+        long y = d*a;
+        
+        float s = 19.2f, u = 3.2f;
+        float g =  s/u;
+        float r = u+s;
+        
+        System.out.println(h);
+        System.out.println(y);
+        System.out.println(g);
+        System.out.println(r);
+    }
+    
+}
