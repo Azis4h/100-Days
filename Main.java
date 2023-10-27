154f360f1892c6c47d17f4f2d8ea4b3fe5cf64ae
azisah <147221473+Azis4h@users.noreply.github.com>
2023-10-27 23:28:06 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+package day19cetakdata;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        // Data yang mau na cetak
+        String nama ="Azisah";
+        int umur    =19;
+        double tinggi =147;
+        
+        // Mencetak data
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur + "tahun");
+        System.out.println("Tinggi: " + tinggi + "cm");
+    }
+    
+}
