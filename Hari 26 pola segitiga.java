2c46d0abf2b9c8aea4367c5c392f9376ce6c8d3e
azisah <147221473+Azis4h@users.noreply.github.com>
2023-11-03 20:32:06 +0800

    Hari 26 pola segitiga.java

---
+++ Hari 26 pola segitiga.java
@@ -0,0 +1,12 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        int pola1 = 10;
+        for(int a = 0; a < pola1; a++){
+            for(int b = 0; b < a; b++ ){
+         System.out.print("#");  
+        }
+        System.out.println("#");
+    }
+    } 
+}
