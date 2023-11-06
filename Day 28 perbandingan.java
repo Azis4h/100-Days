947a0646c142517a769bfddc65b1d149ea3ea4d5
azisah <147221473+Azis4h@users.noreply.github.com>
2023-11-06 17:36:43 +0800

    Day 28 perbandingan.java

--- Day 28 perbandingan.java
+++ Day 28 perbandingan.java
@@ -1,19 +1,21 @@
-package day15perbandingan;
+package perbandingan;
 import java.util.Scanner;
-public class Main {
+public class Perbandingan {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner woo = new Scanner(System.in);
         System.out.print("Masukkan angka :");
-        int number= scanner.nextInt();
+        int number= woo.nextInt();
         
         if (number % 10 == 0 && number % 5 == 0) {
             System.out.println("yaa class");
         } else if (number % 10 == 0) {
             System.out.println(" 10");
         } else if (number % 5 == 0) {
-            System.out.printl (" 5");
+            System.out.print (" 5");
         } else {
-            System.out.println("bodo inie tidak masuk dalam ketentuan angkamu");
+            System.out.println(" tidak masuk dalam ketentuan angkamu");
         }     
+
     }
+    
 }
