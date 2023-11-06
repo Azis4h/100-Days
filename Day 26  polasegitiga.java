8f424f3e157cf4cc843e60539384dbeadb212937
azisah <147221473+Azis4h@users.noreply.github.com>
2023-11-06 17:51:49 +0800

    Day 26  polasegitiga.java

--- Day 26  polasegitiga.java
+++ Day 26  polasegitiga.java
@@ -1,9 +1,9 @@
 package pola.segitiga;
 public class PolaSegitiga {
     public static void main(String[] args) {
-       int pola4 = 5;
-        for (int i = 0; i < pola4; i++) {
-            for (int j = 0; j <= i; j++) {
+       int pola1 = 5;
+        for (int a = 0; a < pola1; i++) {
+            for (int b = 0; b <= a; j++) {
                 System.out.print("*");
             }
             System.out.println("");
