8effdc0a1bf1dce67be204d1439ab8416a6410e7
azisah <147221473+Azis4h@users.noreply.github.com>
2023-11-06 17:52:14 +0800

    Day 26  polasegitiga.java

--- Day 26  polasegitiga.java
+++ Day 26  polasegitiga.java
@@ -6,7 +6,7 @@ public class PolaSegitiga {
             for (int b = 0; b <= a; j++) {
                 System.out.print("*");
             }
-            System.out.println("");
+            System.out.println("*");
         }
 
 
