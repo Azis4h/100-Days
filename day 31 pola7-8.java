347dbb7325d3c811c035e317261047d810580875
azisah <147221473+Azis4h@users.noreply.github.com>
2023-11-08 18:18:43 +0800

    day 31 pola7-8.java

---
+++ day 31 pola7-8.java
@@ -0,0 +1,26 @@
+package pola7.pkg8;
+public class Pola78 {
+    public static void main(String[] args) {
+    int pola7 = 8;
+        for(int a = pola7; a >= 0; a--){
+            for( int b = 0; b < pola7 -a; b++){
+               System.out.print(" ");
+            }
+            for(int b = 0; b <= a; b++){
+                System.out.print("*");
+        }
+            System.out.println(" ");
+    }  
+        int pola8 = 4;
+        for(int a = 0; a < pola8; a--){
+            for( int b = 0; b < pola8 -a; b++){
+               System.out.print(" ");
+            }
+            for(int b = 0; b <= a; b++){
+                System.out.print("* ");
+        }
+            System.out.println(" ");
+}
+    }
+    
+}
