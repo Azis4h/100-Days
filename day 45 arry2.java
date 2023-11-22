ea1191d14b28f5360f4ad32016107c4cae003c93
azisah <147221473+Azis4h@users.noreply.github.com>
2023-11-22 19:17:28 +0800

    day 45 arry2.java

---
+++ day 45 arry2.java
@@ -0,0 +1,19 @@
+package ipu;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int[] a ={10,8,2};
+        String[] hari ={"selasa", "kamis"};
+        System.out.println(hari[1]);
+        System.out.println(a[0]);
+        
+        
+        int c[] = new int[3];
+        c[0] = 10;
+        c[1] = 6;
+        c[2] = 2;
+        System.out.println(c[1]);
+    }
+    
+}
