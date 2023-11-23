2cccc51b8b28af7d92cb42dd6124a009de5bb82f
azisah <147221473+Azis4h@users.noreply.github.com>
2023-11-23 11:26:21 +0800

    day 46 perulangan do while.java

---
+++ day 46 perulangan do while.java
@@ -0,0 +1,14 @@
+package perulangan;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int a = 1;
+        do{
+            System.out.println("hello world");
+            System.out.println("selamat pagi");
+            a++;
+        }while(a <= 5);
+    }
+    
+}
