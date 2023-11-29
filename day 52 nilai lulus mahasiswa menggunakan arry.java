8f386d2581b541bca7ecf800c1099ca8cfba2ea1
azisah <147221473+Azis4h@users.noreply.github.com>
2023-11-29 19:46:51 +0800

    day 52 nilai lulus mahasiswa menggunakan arry.java

---
+++ day 52 nilai lulus mahasiswa menggunakan arry.java
@@ -0,0 +1,15 @@
+package penjumlahan;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int [] nilai = {45,78,71,89,93,58,62,63,34,78};
+        for(int a = 0; a < nilai.length; a++){
+            if(nilai [a] >= 70){
+                System.out.println(nilai [a]);
+            }
+        }
+        System.out.println("");
+    }
+    
+}
