72b51d4c2d22253fb7c7659eaefdab0ce7e4a28d
azisah <147221473+Azis4h@users.noreply.github.com>
2023-12-03 19:37:20 +0800

    day 56 faktorial.java

---
+++ day 56 faktorial.java
@@ -0,0 +1,22 @@
+package faktorial;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int angka = 5;
+        int hasil = angka;
+        System.out.println(angka+"! = ");
+        for(int a = angka; a > 0; a--){
+            System.out.print(a);
+            if(a > 1){
+                System.out.print(" x ");
+            }
+            if(angka != a){
+                hasil *= a;
+            }
+        }
+        System.out.println("");
+        System.out.println("hasil = "+hasil);
+    }
+    
+}
