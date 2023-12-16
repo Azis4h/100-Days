d302f30c779b67badf48b328ef260593b14bf647
azisah <147221473+Azis4h@users.noreply.github.com>
2023-12-16 20:49:12 +0800

    day 69 mencetak arry.java

---
+++ day 69 mencetak arry.java
@@ -0,0 +1,15 @@
+package data;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int [] bilangan ={29,45,14,34,78};
+		             int i =0;
+		
+		              while(i <bilangan.length){
+	                		System.out.println(bilangan [i] );
+			                i++;
+		}
+    }
+    
+}
