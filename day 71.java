d375ca9ff501d28c39f98daf4f5c560ba694c392
azisah <147221473+Azis4h@users.noreply.github.com>
2023-12-18 18:47:28 +0800

    day 71.java

---
+++ day 71.java
@@ -0,0 +1,15 @@
+package cetak2;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int[]nilai ={10,27,80,25,10};
+	        	int angka = 0;
+		         do{
+			            System.out.println(nilai [angka] );
+			                 angka++;
+		            }while(angka < nilai.length);
+		
+    }
+    
+}
