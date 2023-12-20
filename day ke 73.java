ed4797bf68a51b8ef6d2ed8a4b25661fea498d46
azisah <147221473+Azis4h@users.noreply.github.com>
2023-12-20 20:32:08 +0800

    day ke 73.java

---
+++ day ke 73.java
@@ -0,0 +1,20 @@
+package javaapplication148;
+
+import java.util.Scanner;
+
+public class JavaApplication148 {
+    public static void main(String[] args) {
+        
+       while(true){
+           Scanner sc = new Scanner(System.in);
+          System.out.print("masukkan angka : ");
+          int angka = sc.nextInt();
+           if (angka == 30) {
+               break;
+           }
+       }
+              
+         
+    }
+    
+}
