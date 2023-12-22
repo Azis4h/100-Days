c6a45ed9589be0fadb8af2f46da16e39bfc51308
azisah <147221473+Azis4h@users.noreply.github.com>
2023-12-22 19:15:10 +0800

    day ke 75 mengganti isi arry.java

---
+++ day ke 75 mengganti isi arry.java
@@ -0,0 +1,14 @@
+package menentukan;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String buah [] = {"jambu","mangga","apel"};
+        buah [0] = "alpukat";
+        for(int i = 0; i < buah.length; i++){
+            System.out.println(buah[i]);
+        }
+        
+        }
+
+}
