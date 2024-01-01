843a840683db72a475718ee78748cad4c106a343
azisah <147221473+Azis4h@users.noreply.github.com>
2024-01-01 17:31:08 +0800

    day ke 85 tak terhingga.java

---
+++ day ke 85 tak terhingga.java
@@ -0,0 +1,9 @@
+package takterhingga;
+
+public class lingkaran {
+    public static void main(String[] args){
+        for(int i = 3; i > 2; i++){
+            System.out.println(i);
+        }
+}
+}
