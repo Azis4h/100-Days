ae7a449137814a1a78964736666175ac81734c3e
azisah <147221473+Azis4h@users.noreply.github.com>
2024-01-02 11:59:53 +0800

    day ke 86 for arry.java

---
+++ day ke 86 for arry.java
@@ -0,0 +1,11 @@
+package arry;
+
+public class menampilkan {
+    public static void main(String[] args){
+        String benda [] = {"kayu","pisau","buku","pulpen"};
+         
+        for(String a : benda){
+            System.out.println(a);
+        }
+}
+}
