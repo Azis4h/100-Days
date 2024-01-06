ade40d98f7f643eed9e5b18579ac5380b0d5debf
azisah <147221473+Azis4h@users.noreply.github.com>
2024-01-06 09:47:50 +0800

    day ke 90 arry 2D.java

---
+++ day ke 90 arry 2D.java
@@ -0,0 +1,13 @@
+package day90;
+
+public class arry3 {
+    public static void main(String[] args){
+        int nilai [][] = {
+            {34,5,7},
+            {5,8,19}
+        };
+        
+        System.out.print(nilai [0][1]);
+        System.out.print(nilai [1][0]);
+}
+}
