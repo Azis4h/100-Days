aaefc76a895ef084b7df020ab88e7d457937a099
azisah <147221473+Azis4h@users.noreply.github.com>
2024-01-11 18:16:44 +0800

    day ke 95 nilai terbesar arry2D.java

---
+++ day ke 95 nilai terbesar arry2D.java
@@ -0,0 +1,22 @@
+package day95;
+
+public class terbesar {
+    public static void main(String[] args){
+        int nilai [][] = {
+            {43,6,3},
+            {22,10,4},
+            {34,6,1}
+        };
+        int hasil = nilai [0][0];
+        for(int i = 0; i < nilai.length; i++){
+            for(int j = 0; j < nilai[i].length; j++){
+                if(nilai [i][j] > hasil){
+                    hasil = nilai [i][j];
+                }
+            }
+        }
+        System.out.println("nilai terbesar adalah : "+hasil);
+}
+
+}
+
